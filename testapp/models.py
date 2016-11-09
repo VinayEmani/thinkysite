8cf439ef8d9701d1e17624af3294b629dba73ceb
Vinay Emani <vinayemani@gmail.com>
2016-11-09 18:43:42 +0530

    thread_type field added to Thread model

--- testapp/models.py
+++ testapp/models.py
@@ -43,6 +43,7 @@ class Thread(models.Model):
     title = models.CharField(max_length=200)
     posted_by = models.ForeignKey(User, on_delete=models.CASCADE)
     post_date = models.DateTimeField()
+    thread_type = models.IntegerField(default=0)
 
     def __str__(self):
         return self.title
